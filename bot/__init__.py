7495804f43394ef2321002e60a6b4cc9f744eb3f
jayvora19 <75425454+jayvora19@users.noreply.github.com>
2023-05-17 09:11:08 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -11,15 +11,15 @@ from logging.handlers import RotatingFileHandler
 from .translation import Translation
 
 # Change Accordingly While Deploying To A VPS
-APP_ID = int(os.environ.get("APP_ID"))
+APP_ID = int(os.environ.get("26861990"))
 
-API_HASH = os.environ.get("API_HASH")
+API_HASH = os.environ.get("0592761ae3a24dcf709d85ab87bc12b9")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("6296295298:AAG82Ph5DQjGVKwg4vSUyNyuLaW3YXL4A7E")
 
-DB_URI = os.environ.get("DB_URI")
+DB_URI = os.environ.get("mongodb+srv://jayvora:1850@cluster.t5ql7o3.mongodb.net/?retryWrites=true&w=majority")
 
-USER_SESSION = os.environ.get("USER_SESSION")
+USER_SESSION = os.environ.get("BQGZ4aYAb7SbzAcwkRxG_ICO_fcDZMR6BIO3vIKApsfqKWJT-2-_oaEVmxn1CXvx-hi9s14_ennY0WMclgKkTQaFT-aQqN4HSt5BdRQJ6B99Er0pmAR5iij29Axu0RGVP8Zf3YqX7_hHFxYKkPm0MwpEjGvRNQT8ZqQssm2mcMF1O2sM5CI3YA7JgkYuOJeDKtNiyOXr0Y_kKp-tQaiUYkFq-D1jrHoNzHS2oMBU77b10gFiZHIBDD0kd4x9-17HB4OG-33qjcMMUPbFv-dkpYDueIX8HerB0v2yb6OItXiXq7rAuJX_mj5-yGt_eOSq275A4iMUDH1DzeM4WAAaiB4QZAIXvwAAAAFnifBOAA")
 
 VERIFY = {}
 
